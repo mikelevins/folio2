e61ba3e6c47790d77bfdca162ccca4af42ab33eb
mikel evins <mevins@me.com>
2014-01-01 21:37:48 -0600

    minor tweaks to the generator example

--- examples/name-generator.lisp
+++ examples/name-generator.lisp
@@ -11,41 +11,30 @@
 
 (in-package :folio)
 
-(defparameter long-enough? (compose (partial 'cl:<= 3) 'length))
+(defparameter +maximum-name-length+ 12)
 
-(defmethod triples ((s cl:sequence)) 
-  (take-by 3 1 s))
+(defun pending? (nm)(right nm))
+(defun name-part (nm)(left nm))
+(defmethod triples ((s cl:sequence)) (take-by 3 1 s))
 
-(defmethod read-names ((path pathname)) 
-  (lines path))
+(defmethod read-names ((path pathname)) (lines path))
 
 (defmethod read-names ((path string)) 
-  (filter long-enough?
+  (filter (compose (partial 'cl:<= 3) 'length)
           (as 'cl:sequence (read-names (pathname path)))))
-
-;;; (read-names "/Users/mikel/Workshop/folio2/examples/namefiles/dickens.names")
 
 (defmethod rules ((names cl:sequence))
   (multiple-value-bind (starts parts)(dispose (image 'triples names) 'left 'right)
     (cl:values starts (reduce 'append parts))))
-
-(defparameter +maximum-name-length+ 12)
-
-(defun pending? (nm)
-  (right nm))
-
-(defun name-part (nm)
-  (left nm))
 
 (defun find-extension (nm parts)
   (any (filter (partial 'prefix-match? (leave 2 nm))
                parts)))
 
 (defun merge-name (nm segment)
-  (case (length segment)
-    (0 nm)
-    ((1 2) (append nm segment))
-    (t (append nm (drop 2 segment)))))
+  (if  (< (length segment) 3)
+       (append nm segment)
+       (append nm (drop 2 segment))))
 
 (defun make-name-builder (parts)
   (lambda (start)
@@ -64,17 +53,16 @@
                                    (iterate (make-name-builder parts)
                                        (pair (any starts) t)))))))
 
-;;; (multiple-value-setq ($starts $parts) (rules (read-names "/Users/mikel/Workshop/folio2/examples/namefiles/dickens.names")))
-;;; (build-name $starts $parts)
 
 (defmethod names ((path string) &optional (number 10))
   (multiple-value-bind (starts parts)(rules (read-names path))
     (as 'cl:list (take number (repeat (build-name starts parts))))))
 
 
+;;; (multiple-value-setq ($starts $parts) (rules (read-names "/Users/mikel/Workshop/folio2/examples/namefiles/dickens.names")))
+;;; (build-name $starts $parts)
 ;;; (names "/Users/mikel/Workshop/folio2/examples/namefiles/dickens.names" 10)
 ;;; (dolist (nm (names "/Users/mikel/Workshop/folio2/examples/namefiles/dickens.names" 30) (terpri))(terpri)(princ nm))
 ;;; (dolist (nm (names "/Users/mikel/Workshop/folio2/examples/namefiles/gnome-female.names" 30) (terpri))(terpri)(princ nm))
 ;;; (dolist (nm (names "/Users/mikel/Workshop/folio2/examples/namefiles/gnome-male.names" 30) (terpri))(terpri)(princ nm))
 ;;; (dolist (nm (names "/Users/mikel/Workshop/folio2/examples/namefiles/us.names" 30) (terpri))(terpri)(princ nm))
-;;; (dolist (nm (names "/usr/share/dict/words" 30) (terpri))(terpri)(princ nm))
